1317184c08548d42ca1c6a78e4630958b78e5eac
Hasan Hacioglu <asanhix@gmail.com>
2019-09-28 16:36:02 +0300

    part 3-7

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 let persons = [
   {
@@ -22,6 +23,7 @@ let persons = [
 
 app.use(bodyParser.json()
 )
+app.use(morgan('tiny'))
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
