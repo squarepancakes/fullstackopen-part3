34d94dc64f9e1f3d0df6fa5499223856b133ce52
Hasan Hacioglu <asanhix@gmail.com>
2019-09-28 15:00:51 +0300

    part 3-2

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  response.send(`
+    <div>Phonebook has info for ${persons.length} people</div>
+    <div>${new Date().toString()}</div>
+  `)
+} )
+
 app.listen(3001)
 
 console.log(`App is running on port 3001`)
